362fe242ddd02ad442701ef9f93f3025c5eb8853
Travis <travis.moulton002@gmail.com>
2022-01-02 09:38:44 -0500

    added test utils file

---
+++ src/shared/testUtils.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import authReducer from '../store/authSlice';
+
+function customRender(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: {
+        auth: authReducer,
+      },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {}
+) {
+  function wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return render(ui, { wrapper, ...renderOptions });
+}
+
+export const createSpy = (obj, methodName, returnVal) =>
+  jest.spyOn(obj, methodName).mockImplementation(jest.fn(() => returnVal));
+
+export * from '@testing-library/react';
+
+export { customRender };
