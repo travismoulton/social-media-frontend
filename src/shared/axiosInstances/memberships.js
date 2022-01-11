676979af00f98e072a0d0d69f9deb0f7450d58d4
Travis <travis.moulton002@gmail.com>
2022-01-11 16:28:04 -0500

    set proxy in package.json. Changed axios instances to use a relative URL

--- src/shared/axiosInstances/memberships.js
+++ src/shared/axiosInstances/memberships.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const membershipInstance = axios.create({
-  baseURL: 'https://127.0.0.1:8080/membership/',
+  // baseURL: 'https://127.0.0.1:8080/membership/',
+  baseURL: '/membership',
   timeout: 5000,
   withCredentials: true,
 });
