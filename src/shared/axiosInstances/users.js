975f68ebf55845607b9f6027f2c39e16792b8aea
Travis <travis.moulton002@gmail.com>
2022-01-11 06:55:45 -0500

    in package.json, changed start script to use https. Inside axios instances, update url to include https

--- src/shared/axiosInstances/users.js
+++ src/shared/axiosInstances/users.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `http://127.0.0.1:8080/users/`,
+  baseURL: `https://127.0.0.1:8080/users/`,
   timeout: 5000,
   withCredentials: true,
 });
