2430dc38cd4268c22e8e608b77660bef7f99a179
Travis <travis.moulton002@gmail.com>
2022-01-18 14:23:51 -0500

    wrote tests for CreateGroup

--- src/components/CreateGroup/CreateGroup.test.js
+++ src/components/CreateGroup/CreateGroup.test.js
@@ -1,4 +1,9 @@
-import { customRender, createSpy, screen } from '../../shared/testUtils';
+import {
+  customRender,
+  createSpy,
+  screen,
+  fireEvent,
+} from '../../shared/testUtils';
 import CreateGroup from './CreateGroup';
 import { createGroupUtils as utils } from './createGroupUtils';
 
@@ -30,5 +35,36 @@ describe('<CreateGroup />', () => {
     setup();
 
     expect(screen.getByText('Create Group')).toBeInTheDocument();
+  });
+
+  test('displays error message if there GroupName input is left empty', () => {
+    setup();
+    const btn = screen.getByText('Create Group');
+
+    fireEvent.click(btn);
+
+    expect(screen.getByText('Group name is required')).toBeInTheDocument();
+    expect(mockCreateGroup).not.toBeCalled();
+  });
+
+  test('send api request when form is filled out correctly', () => {
+    setup();
+    const btn = screen.getByText('Create Group');
+
+    const nameInput = screen.getByLabelText('Group Name');
+    fireEvent.change(nameInput, { target: { value: 'fake name' } });
+
+    const descriptionInput = screen.getByTestId('groupDescription');
+    fireEvent.change(descriptionInput, {
+      target: { value: 'fake description' },
+    });
+
+    fireEvent.click(btn);
+    expect(mockCreateGroup).toBeCalledWith(
+      expect.objectContaining({
+        description: 'fake description',
+        name: 'fake name',
+      })
+    );
   });
 });
