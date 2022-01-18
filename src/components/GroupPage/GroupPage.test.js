d42767b5e5d3923d6eb39d3fec5a10420c9a3d11
Travis <travis.moulton002@gmail.com>
2022-01-18 15:07:56 -0500

    working on tests for GroupPage

---
+++ src/components/GroupPage/GroupPage.test.js
@@ -0,0 +1,61 @@
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+import { groupPageUtils as utils } from './groupPageUtils';
+import {
+  customRender,
+  screen,
+  createSpy,
+  waitFor,
+} from '../../shared/testUtils';
+import GroupPage from './GroupPage';
+
+jest.mock('./groupPageUtils');
+
+describe('<GroupPage />', () => {
+  async function setup() {
+    const history = createMemoryHistory();
+    history.location.state = { groupId: 'groupId' };
+
+    const preloadedState = {
+      auth: { user: { groupMemberships: ['groupId'] } },
+    };
+
+    customRender(
+      <Router history={history}>
+        <GroupPage />
+      </Router>,
+      { preloadedState }
+    );
+  }
+
+  let mockFetchGroup;
+
+  const date = Date.now();
+  const mockGroup = {
+    name: 'fake group',
+    createdAt: date,
+    description: 'fake description',
+    memberCount: 10,
+    _id: 'groupId',
+  };
+
+  beforeEach(() => {
+    mockFetchGroup = createSpy(
+      utils,
+      'fetchGroup',
+      Promise.resolve({ data: { Group: mockGroup } })
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    mockFetchGroup = null;
+  });
+
+  test('renders', async () => {
+    await waitFor(() => setup());
+
+    expect(screen.getAllByText('fake group', { exact: false })).toHaveLength(2);
+  });
+});
--- src/components/GroupPage/groupPageUtils.js
+++ src/components/GroupPage/groupPageUtils.js
@@ -2,7 +2,9 @@ import axios from '../../shared/axiosInstances/groups';
 
 export const groupPageUtils = {
   fetchGroup: async function (groupId) {
-    const { data } = await axios.get(`/${groupId}`);
+    const { data } = await axios
+      .get(`/${groupId}`)
+      .catch((err) => console.log(err));
 
     return data;
   },
