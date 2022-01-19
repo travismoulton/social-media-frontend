12044220d3d0f386b158a0f1f191626cd6c5bfb6
Travis <travis.moulton002@gmail.com>
2022-01-19 08:40:31 -0500

    wrote testing fro AboutGroup

---
+++ src/components/GroupPage/AboutGroup/AboutGroup.test.js
@@ -0,0 +1,53 @@
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+import {
+  customRender,
+  screen,
+  fireEvent,
+  waitFor,
+} from '../../../shared/testUtils';
+import AboutGroup from './AboutGroup';
+
+const mockGroup = {
+  name: 'fake group',
+  createdAt: new Date('January 19, 2022'),
+  description: 'fake description',
+  memberCount: 10,
+  _id: 'groupId',
+};
+
+describe('<AboutGroup />', () => {
+  test('renders and displays the correct date', () => {
+    const history = createMemoryHistory();
+
+    customRender(
+      <Router history={history}>
+        <AboutGroup group={mockGroup} />
+      </Router>
+    );
+
+    expect(screen.getByText('About fake group')).toBeInTheDocument();
+    expect(screen.getByText('Created Jan 19, 2022')).toBeInTheDocument();
+    expect(screen.getByText('Number of members: 10')).toBeInTheDocument();
+    expect(screen.getByText('Create Post')).toBeInTheDocument();
+  });
+
+  test('create post btn redirects to the right page', async () => {
+    const history = createMemoryHistory();
+
+    customRender(
+      <Router history={history}>
+        <AboutGroup group={mockGroup} />
+      </Router>
+    );
+
+    const btn = screen.getByText('Create Post');
+
+    fireEvent.click(btn);
+
+    await waitFor(() =>
+      expect(history.location.pathname).toBe('/group/fake-group/createThread')
+    );
+  });
+});
