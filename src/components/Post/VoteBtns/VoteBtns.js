4560a72172bd371dca40353577e77f50eb82bf84
Travis <travis.moulton002@gmail.com>
2022-01-26 18:41:42 -0500

    deleted old commented code from VoteBtns

--- src/components/Post/VoteBtns/VoteBtns.js
+++ src/components/Post/VoteBtns/VoteBtns.js
@@ -13,44 +13,12 @@ export default function VoteBtns({ post: postData, vertical }) {
   const { user } = useSelector((state) => state.auth);
 
   async function likeHandler() {
-    // If the user already likes the post:
     if (post.usersLiked.includes(user._id)) {
-      // 1: Make an API call to remove the like from the database
       const { data } = await removeLike(post._id);
       setPost(data.post);
-
-      // const userIndex = post.usersLiked.indexOf(user._id);
-
-      // setPost({
-      //   ...post,
-      //   usersLiked: post.usersLiked
-      //     .splice(0, userIndex)
-      //     .concat(post.usersLiked.splice(userIndex + 1)),
-      //   likeScore: post.likeScore - 1,
-      // });
     } else {
       const { data } = await addLike(post._id);
       setPost(data.post);
-
-      // if (post.usersDisliked.includes(user._id)) {
-      //   const userIndex = post.usersDisliked.indexOf(user._id);
-
-      //   setPost({
-      //     ...post,
-
-      //     usersLiked: [...post.usersLiked, user._id],
-      //     usersDisliked: post.usersDisliked
-      //       .splice(0, userIndex)
-      //       .concat(post.usersDisliked.splice(userIndex + 1)),
-      //     likeScore: post.likeScore + 2,
-      //   });
-      // } else {
-      //   setPost({
-      //     ...post,
-      //     likeScore: post.likeScore + 1,
-      //     usersLiked: [...post.usersLiked, user._id],
-      //   });
-      // }
     }
   }
 
@@ -58,40 +26,9 @@ export default function VoteBtns({ post: postData, vertical }) {
     if (post.usersDisliked.includes(user._id)) {
       const { data } = await removeDislike(post._id);
       setPost(data.post);
-
-      // const userIndex = post.usersDisliked.indexOf(user._id);
-
-      // setPost({
-      //   ...post,
-      //   usersDisliked: post.usersDisliked
-      //     .splice(0, userIndex)
-      //     .concat(post.usersDisliked.splice(userIndex + 1)),
-      //   likeScore: post.likeScore - 1,
-      // });
     } else {
       const { data } = await addDislike(post._id);
       setPost(data.post);
-
-      // // If the user likes the post already, we need to modify the likeCount,
-      // //  and usersLiked properties as well as the dislikeCount and usersDisliked
-      // if (post.usersLiked.includes(user._id)) {
-      //   const userIndex = post.usersLiked.indexOf(user._id);
-
-      //   setPost({
-      //     ...post,
-      //     usersDisliked: [...post.usersDisliked, user._id],
-      //     usersLiked: post.usersLiked
-      //       .splice(0, userIndex)
-      //       .concat(post.usersLiked.splice(userIndex + 1)),
-      //     likeScore: post.likeScore - 2,
-      //   });
-      // } else {
-      //   setPost({
-      //     ...post,
-      //     likeScore: post.likeScore - 1,
-      //     usersDisliked: [...post.usersDisliked, user._id],
-      //   });
-      // }
     }
   }
 
