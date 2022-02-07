855671b73dd42ad550e34b8f8eac947a9253fa0e
Travis <travis.moulton002@gmail.com>
2022-02-07 13:34:49 -0500

    fixed bug in App.js keeping the NavBar appearing as there was a user after execuiting a logout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './App.css';
 import Login from './components/Login/Login';
@@ -22,20 +22,25 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  const { user } = useSelector((state) => state.auth);
+
   useEffect(() => {
     (async () => {
       // When the app is loaded, checkForUser will send a request to the API
       // If there is a valid token on the request, coresponding user data will
       // be sent back, and set in the store.
       const userToLogin = await checkForUser();
-      if (userToLogin) {
-        dispatch(authSuccess(userToLogin));
-        setIsAuthenticated(true);
-      }
+      if (userToLogin) dispatch(authSuccess(userToLogin));
 
       setLoading(false);
     })();
   }, [dispatch]);
+
+  // Anytime the user logs in or out, this will trigger
+  // and set isAuthenitcated state appropriatley
+  useEffect(() => {
+    setIsAuthenticated(!!user);
+  }, [user]);
 
   const routes = (
     <Switch>
