79ca364b9bda5decb9a54980c42b27637775c517
Travis <travis.moulton002@gmail.com>
2022-02-11 18:33:51 -0500

    updated tests for Post component

--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -7,9 +7,11 @@ import {
 } from '../../shared/testUtils';
 import { mockInitialPost } from '../../shared/mockThread';
 import { submitReplyBtnUtils as utils } from './SubmitReplyBtn/submitReplyBtnUtils';
+import { postUtils } from './postUtils';
 import Post from './Post';
 
 jest.mock('./SubmitReplyBtn/submitReplyBtnUtils.js');
+jest.mock('./postUtils');
 
 describe('<Post />', () => {
   function setup(userId, post) {
@@ -17,26 +19,62 @@ describe('<Post />', () => {
 
     const props = { post, reloadThread: jest.fn() };
 
-    customRender(<Post {...props} />, { preloadedState });
+    // Render the component without a user if null is passed for the userId
+    userId
+      ? customRender(<Post {...props} />, { preloadedState })
+      : customRender(<Post {...props} />);
 
     return props.reloadThread;
   }
 
-  test('renders, and shows vote button if user is not post author', () => {
+  test('renders, and shows vote button if there is a user, and does not render edit and delete buttons if the user is not the author', () => {
     setup('mockUser2', mockInitialPost);
 
     expect(screen.getByText('mockUser1')).toBeInTheDocument();
     expect(screen.getByText('mock post 1 content')).toBeInTheDocument();
     expect(screen.getByTestId('VoteBtns')).toBeInTheDocument();
+
+    expect(screen.queryByText('Delete post')).not.toBeInTheDocument();
+    expect(screen.queryByText('Edit post')).not.toBeInTheDocument();
   });
 
-  test('renders and does not show to vote btns if user is author', () => {
-    setup('mockUser1', mockInitialPost.replies[0]);
+  test('renders and does not show to vote btns if there is no user', () => {
+    setup(null, mockInitialPost.replies[0]);
 
     expect(screen.getByText('mockUser1')).toBeInTheDocument();
     expect(screen.getByText('mock reply 1 content')).toBeInTheDocument();
 
     expect(screen.queryByTestId('VoteBtns')).not.toBeInTheDocument();
+  });
+
+  test('delete post button makes api call to delete the post', async () => {
+    const mockDeletePost = createSpy(
+      postUtils,
+      'deletePost',
+      Promise.resolve({})
+    );
+
+    setup('mockUser1', mockInitialPost);
+
+    fireEvent.click(screen.getByText('Delete post'));
+
+    await waitFor(() => expect(mockDeletePost).toBeCalled());
+  });
+
+  test('when the user clicks edit post, and then changes the post and clicks submit, editPost api call is made', async () => {
+    const mockEditPost = createSpy(utils, 'editPost', Promise.resolve({}));
+
+    setup('mockUser1', mockInitialPost);
+
+    fireEvent.click(screen.getByText('Edit post'));
+
+    const textArea = screen.getByTestId('createReply');
+
+    fireEvent.change(textArea, { target: { value: 'Reply content edited' } });
+
+    fireEvent.click(screen.getByText('Submit changes'));
+
+    await waitFor(() => expect(mockEditPost).toBeCalled());
   });
 
   test('reply input is only shown after the button is clicked', () => {
@@ -53,7 +91,11 @@ describe('<Post />', () => {
   test('if there is replyInput, the SubmitReplyBtn makes the API call and closes the reply box', async () => {
     const reloadThread = setup('mockUser2', mockInitialPost);
 
-    let mockSubmitReply = createSpy(utils, 'submitReply', Promise.resolve({}));
+    const mockSubmitReply = createSpy(
+      utils,
+      'submitReply',
+      Promise.resolve({})
+    );
 
     const replyBtn = screen.getByText('Reply', { exact: false });
     fireEvent.click(replyBtn);
@@ -77,6 +119,19 @@ describe('<Post />', () => {
     await waitFor(() => expect(reloadThread).toBeCalled());
 
     // The text area should be closed after the post is submitted
+    expect(textArea).not.toBeInTheDocument();
+  });
+
+  test('When in the ReplyInput is open, clicking the cancel button closes it', () => {
+    setup('mockUser2', mockInitialPost);
+
+    fireEvent.click(screen.getByText('Reply'));
+
+    const textArea = screen.getByTestId('createReply');
+    expect(textArea).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Cancel'));
+
     expect(textArea).not.toBeInTheDocument();
   });
 });
